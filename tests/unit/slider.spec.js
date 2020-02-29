3f468ea0c6c1ef25a9ae3c5e1f513fd6f7b43baa
Dmitry Verteyko <verteyko1990@gmail.com>
2020-02-29 12:35:28 +0200

    unit testing

---
+++ tests/unit/slider.spec.js
@@ -0,0 +1,14 @@
+import { shallowMount } from '@vue/test-utils';
+import vSlider from '../../src/components/v-slider';
+
+// describe('vSlider.vue', () => {
+//   it('renders props information when passed', () => {
+//     const interval = 3000;
+//     const wrapper = shallowMount(vSlider, {
+//       propsData: {
+//         interval: 3000
+//       },
+//     });
+//     expect(wrapper.props()).toBe(3000);
+//   });
+// });
